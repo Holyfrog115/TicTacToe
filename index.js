9d723bfe38607b02028c2c63d3f764bc6c63e1e1
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-08-06 20:25:49 +0700

    Add factory function for making gameboard object
    Gameboard object has board itself as array of numbers, reset, print and
    check for win/tie methods.

---
+++ index.js
@@ -0,0 +1,70 @@
+function Gameboard() {
+    let board = [];
+
+    const resetBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            board.push(-1);
+        }
+    }
+
+    const printBoard = () => {
+        // Console board
+
+        for (let i = 0; i < 9; i += 3) { 
+            console.log(board[i], board[i + 1], board[i + 2]);
+        }
+    }
+
+    const checkGameStatus = () => {
+        // Checks if there is a winner or tie
+
+        let isWinned = 0;
+
+        // Horizontal winner check
+        for (let i = 0; i < 9; i += 3) { 
+            if (board[i] != -1 && (board[i] === board[i + 1] && board[i + 1] === board[i + 2])) {
+                isWinned = 1;
+                break;
+            }
+        }
+
+        // Vertical winner check
+        if (!isWinned) {
+            for (let i = 0; i < 3; i++) {
+                if (board[i] != -1 && (board[i] === board[i + 3] && board[i + 3] === board[i + 6])) {
+                    isWinned = 1;
+                    break;
+                }
+            }
+        }
+
+        // Diagonal winner check
+        if (!isWinned) {
+            const diagonal1 = (board[0] != -1 && (board[0] === board[4] && board[4] === board[8]));
+            const diagonal2 = (board[0] != -1 && (board[2] === board[4] && board[4] === board[6]));
+            if (diagonal1 || diagonal2) {
+                isWinned = 1;
+            }
+        }
+
+        if (isWinned) {
+            return isWinned;
+        }
+
+        // Tie check
+
+        for (cell of board) {
+            if (cell == -1) {
+                return 0;
+            }
+        }
+
+        return 2;
+    }
+
+    return {
+        resetBoard,
+        printBoard,
+        checkGameStatus,
+    };
+}
